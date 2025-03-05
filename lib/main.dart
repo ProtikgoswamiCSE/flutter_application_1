5615b90b3058df81a4ab12de875d46aa9ad5a141
ProtikgoswamiCSE <goswami15-5841@diu.edu.bd>
2025-03-05 16:17:07 +0600

    new 1

--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      color: Color.fromARGB(166, 7, 167, 119),
       home: DefaultTabController(
         length: 3,
         child: Scaffold(
+          backgroundColor: Colors.lightGreenAccent,
           appBar: AppBar(
+            actions: [Icon(Icons.settings)],
             title: Text('Tab Example'),
             bottom: TabBar(
               tabs: [Tab(text: "Tab1"), Tab(text: "Tab2"), Tab(text: "Tab3")],
@@ -25,8 +29,11 @@ class MyApp extends StatelessWidget {
                   child: Text('Content of Tab 1'),
                 ),
               ),
+              Center(
+                child: TextButton.icon(onPressed: () {}, label: Text('Protik')),
+              ),
               Center(child: Text('Content of Tab 2')),
-              Center(child: Text('Content of Tab 2')),
+              Center(child: Text('Content of Tab 3')),
             ],
           ),
         ),
